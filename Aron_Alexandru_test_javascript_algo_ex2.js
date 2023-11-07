febccd744600e15be90b6fff43dd754f9ac1022f
NoraXela <aron.alexlucian@yahoo.com>
2023-11-07 18:22:57 +0200

    V02

--- Aron_Alexandru_test_javascript_algo_ex2.js
+++ Aron_Alexandru_test_javascript_algo_ex2.js
@@ -1,5 +1,4 @@
-/* Veriry all are numbers if numeric and if different from each other */
-
+//* Veriry all are numbers if numeric and if different from each other - FOR LINES
 function verifyPosition(tableRow) {
   var flag = 0;
   for (var i = 0; i < 18; i += 2) {
@@ -23,6 +22,52 @@ function verifyPosition(tableRow) {
     for (var i = 0; i < 18; i += 2) {
       var counter = 0;
       for (j = 0; j < 18; j += 2) {
+        if (tableRow[i] === tableRow[j]) {
+          counter++;
+        }
+        if (counter > 1) {
+          flag = 1;
+          break;
+        }
+      }
+      if (flag === 1) {
+        break;
+      }
+    }
+  }
+
+  if (flag === 1) {
+    return false;
+  } else {
+    return true;
+  }
+}
+//*/
+
+/* Veriry all are numbers if numeric and if different from each other - FOR COLUMNS */
+function verifyPositionCols(tableRow) {
+  var flag = 0;
+  for (var i = 0; i < 9; i += 1) {
+    if (
+      tableRow[i] !== "1" &&
+      tableRow[i] !== "2" &&
+      tableRow[i] !== "3" &&
+      tableRow[i] !== "4" &&
+      tableRow[i] !== "5" &&
+      tableRow[i] !== "6" &&
+      tableRow[i] !== "7" &&
+      tableRow[i] !== "8" &&
+      tableRow[i] !== "9"
+    ) {
+      flag = 1;
+      break;
+    }
+  }
+
+  if (flag === 0) {
+    for (var i = 0; i < 9; i += 1) {
+      var counter = 0;
+      for (j = 0; j < 9; j += 1) {
         if (tableRow[i] === tableRow[j]) {
           counter++;
         }
--- Aron_Alexandru_test_javascript_algo_ex3.js
+++ Aron_Alexandru_test_javascript_algo_ex3.js
@@ -1,12 +1,12 @@
-/* Function for creating error table */
-function createErrorTable(lineNo, errObject) {
+/* Function for creating line error table */
+function createLineErrorTable(lineNo) {
   const table = document.createElement("table");
 
   // Create table data row
   const dataRow = document.createElement("tr");
   const dataCell = document.createElement("td");
   // Line X incorrect
-  dataCell.textContent = errObject + " " + (lineNo + 1) + " incorrect";
+  dataCell.textContent = "Line " + (lineNo + 1) + " incorrect";
   dataRow.appendChild(dataCell);
   for (var j = 0; j < 9; j++) {
     const dataCell = document.createElement("td");
@@ -18,20 +18,133 @@ function createErrorTable(lineNo, errObject) {
   return table;
 }
 
+/* Function for creating column error table */
+function createColumnErrorTable(entityNo, errObject) {
+  const table = document.createElement("table");
+
+  // Create table data row
+  const dataRow = document.createElement("tr");
+  const dataCell = document.createElement("td");
+  // Line X incorrect
+  dataCell.textContent = errObject + " " + entityNo + " incorrect";
+  dataRow.appendChild(dataCell);
+  for (var j = 0; j < 9; j++) {
+    const dataCell = document.createElement("td");
+    dataCell.textContent = colLine[j];
+    dataRow.appendChild(dataCell);
+  }
+  table.appendChild(dataRow);
+
+  return table;
+}
+
+/* Function for creating column error table */
+function createSuccessfulTable() {
+  const table = document.createElement("table");
+
+  // Create table data row
+  const dataRow = document.createElement("tr");
+  const dataCell = document.createElement("td");
+  // Line X incorrect
+  dataCell.textContent = "The table is correct";
+  dataRow.appendChild(dataCell);
+  table.appendChild(dataRow);
+
+  return table;
+}
+
 /* Function for checking lines */
 function checkLines() {
+  flag = 0;
   for (var i = 0; i < 9; i++) {
     console.log(array_number[i]);
     console.log(verifyPosition(array_number[i]));
     if (verifyPosition(array_number[i]) === false) {
-      const table2 = createErrorTable(i, "Line");
+      const table2 = createLineErrorTable(i);
       table2.setAttribute("border", "2");
 
       const tableContainer2 = document.getElementById("table-container2");
       tableContainer2.appendChild(table2);
+      flagGlobal = 1;
     }
   }
 }
 
-/* Checks to see if all lines are correct */
+/* Function for checking columns */
+function checkColumns() {
+  flag = 0;
+  for (var i = 0; i < 18; i += 2) {
+    colLine = "";
+    for (var j = 0; j < 9; j += 1) {
+      colLine = colLine + array_number[j][i];
+    }
+    /*console.log(colLine);*/
+    if (verifyPositionCols(colLine) === false) {
+      const table2 = createColumnErrorTable(i / 2 + 1, "Column");
+      table2.setAttribute("border", "2");
+
+      const tableContainer2 = document.getElementById("table-container2");
+      tableContainer2.appendChild(table2);
+      flagGlobal = 1;
+    }
+  }
+}
+
+/* Function for checking regions */
+function checkEntity(
+  entityName,
+  entityNo,
+  startCol,
+  endCol,
+  startLine,
+  endLine
+) {
+  colLine = "";
+  for (var i = startCol; i < endCol; i += 2) {
+    for (var j = startLine; j < endLine; j += 1) {
+      colLine = colLine + array_number[j][i];
+    }
+  }
+  console.log(colLine);
+  if (verifyPositionCols(colLine) === false) {
+    const table2 = createColumnErrorTable(entityNo, entityName);
+    table2.setAttribute("border", "2");
+
+    const tableContainer2 = document.getElementById("table-container2");
+    tableContainer2.appendChild(table2);
+  }
+}
+
+flagGlobal = 0;
+/* Checks to see if all lines are correct  */
 checkLines();
+/* Checks to see if all columns are correct */
+checkColumns();
+/* Checks to see if region 1 is correct -- fill in start column * 2, (end column + 1) * 2, start line and end line */
+checkEntity("Region", 1, 0, 6, 0, 3);
+/* Checks to see if region 2 is correct  */
+checkEntity("Region", 2, 6, 12, 0, 3);
+/* Checks to see if region 3 is correct  */
+checkEntity("Region", 3, 12, 18, 0, 3);
+/* Checks to see if region 4 is correct */
+checkEntity("Region", 4, 0, 6, 3, 6);
+/* Checks to see if region 5 is correct */
+checkEntity("Region", 5, 6, 12, 3, 6);
+/* Checks to see if region 6 is correct */
+checkEntity("Region", 6, 12, 18, 3, 6);
+/* Checks to see if region 7 is correct */
+checkEntity("Region", 7, 0, 6, 6, 9);
+/* Checks to see if region 8 is correct */
+checkEntity("Region", 8, 6, 12, 6, 9);
+/* Checks to see if region 9 is correct */
+checkEntity("Region", 9, 12, 18, 6, 9);
+
+console.log("flagGlobal " + flagGlobal);
+if (flagGlobal === 0) {
+  const table2 = createSuccessfulTable();
+  table2.setAttribute("border", "2");
+
+  const tableContainer2 = document.getElementById("table-container2");
+  tableContainer2.appendChild(table2);
+  flagGlobal = 1;
+}
